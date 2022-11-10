738286e41669a0139282364435ea7d1ea99ea016
wrangman <wrangman@gmail.com>
2022-11-10 09:35:12 +0100

    new

---
+++ coloring.py
@@ -0,0 +1,18 @@
+import os
+from colors import bcolors         #you import the color values from this class
+
+
+
+os.system('cls')
+
+print("Ingen färg")
+
+
+
+
+name = input(bcolors.YELLOW + "Skriv ditt namn: ")      #You can use colors for both input and print - no problem!
+
+
+
+
+print(bcolors.DEFAULT)              # Restore default color to system
